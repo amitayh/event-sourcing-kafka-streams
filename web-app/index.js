370ca1187a78a7f54d454e198a1c60e9e5a7721b
Amitay Horwitz <amitayh@gmail.com>
2018-08-05 21:04:34 -0400

    wip

--- web-app/index.js
+++ web-app/index.js
@@ -2,31 +2,31 @@ import 'babel-core/register';
 import 'babel-polyfill';
 import express from 'express';
 import {Server} from 'http';
-import io from 'socket.io';
-import {startConsumer} from './server/kafka';
-import {initCommandHandler} from './server/socket';
-import api from './server/api';
+//import io from 'socket.io';
+//import {startConsumer} from './server/kafka';
+//import {initCommandHandler} from './server/socket';
+//import api from './server/api';
 
 const app = express();
 const server = Server(app);
-const socket = io(server);
+//const socket = io(server);
 
 app.use(express.static('client/build'));
-app.use('/api', api);
+//app.use('/api', api);
 
 // WS
-const commandHandler = initCommandHandler(socket);
-startConsumer('invoice-command-results', (id, result) => {
-  Object.keys(result).forEach(key => {
-    const handler = commandHandler[key];
-    if (handler) {
-      handler(result[key]);
-    }
-  });
-});
-startConsumer('invoice-records', (id, record) => {
-  socket.sockets.emit('invoice-updated', {...record, id});
-});
+//const commandHandler = initCommandHandler(socket);
+//startConsumer('invoice-command-results', (id, result) => {
+//  Object.keys(result).forEach(key => {
+//    const handler = commandHandler[key];
+//    if (handler) {
+//      handler(result[key]);
+//    }
+//  });
+//});
+//startConsumer('invoice-records', (id, record) => {
+//  socket.sockets.emit('invoice-updated', {...record, id});
+//});
 
 // socket.on('connection', client => {
 //   console.log('a user connected');
@@ -35,7 +35,7 @@ startConsumer('invoice-records', (id, record) => {
 //   });
 // });
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 server.listen(port, () => {
   console.log(`listening on *:${port}`);
 });
--- web-app/server/api.js
+++ web-app/server/api.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {getInvoices} from './projection';
+//import {getInvoices} from './projection';
 import {executeCommand} from './kafka';
 import {setSocketId} from './socket';
 
 const app = express();
 
-app.get('/invoices', async (req, res) => {
-  res.json(await getInvoices());
+app.get('/invoices', (req, res) => {
+  //res.json(await getInvoices());
+  res.json([{
+    id: '917f7417-2581-4bff-b222-5d4e2cce6588',
+    customerName: 'Amitay Horwitz',
+    customerEmail: 'amitayh@gmail.com',
+    issueDate: '2018-08-05',
+    dueDate: '2018-09-04',
+    total: 12.34,
+    status: 'New'
+  }]);
 });
 
 app.post('/execute', bodyParser.json(), async (req, res) => {
